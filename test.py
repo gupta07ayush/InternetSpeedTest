a7ec218da811f4766714f36dbf092eb7ac5a56a6
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-27 23:48:41 +0530

    comments and documentation

--- test.py
+++ test.py
@@ -8,24 +8,26 @@ root.config(bg="blue")
 
 
 def speedcheck():
-    s = speedtest.Speedtest()
+    s = speedtest.Speedtest()  # s is the object of speedtest module
     s.get_servers()
     
-    down = s.download()
-    down = round(down / (10**6), 2)
+    down = s.download() # gives download speed in bits
+    down = round(down / (10**6), 2)  # converts MBps into Mbps
     down = str(down) + " Mbps"
     
-    up = str(round(s.upload() / (10**6),2)) + " Mbps"
+    up = str(round(s.upload() / (10**6),2)) + " Mbps"  # use single line to do the same to down which took 3 lines
     
+    # sends down and up test results to tkinter label
     down_speed_result.config(text=down)
     up_speed_result.config(text=up)
     
+    # Gives url of the image which contains the test results.
     result_img = s.results.share()
     print(result_img)
     
 
-up_speed_lab = Label(root, text="Internet Speed Test", font=('Times new roman', 30, 'bold'), bg='blue' )
-up_speed_lab.place(x=60, y=40, height=50, width=380)
+heading_lab = Label(root, text="Internet Speed Test", font=('Times new roman', 30, 'bold'), bg='blue' )
+heading_lab.place(x=60, y=40, height=50, width=380)
 
 up_speed_lab = Label(root, text="Download Speed", font=('Times new roman', 30, 'bold', ))
 up_speed_lab.place(x=40, y=130, height=50, width=380)
