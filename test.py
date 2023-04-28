f3c03fc00dd0605e72ad25012976c45c8a07f8b6
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-28 11:23:34 +0530

    changed the UI feel,color, design

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import speedtest
 
 root = Tk()  # object of tkinter module
 root.title("Internet Speed Meter")
-root.geometry('500x700')  # default size of tkinter window
-root.config(bg="blue")  # background color of the window
+root.geometry('600x600')  # default size of tkinter window
+root.config(bg="#1b4965")  # background color of the window
 
 
 def speedcheck():
@@ -28,29 +28,32 @@ def speedcheck():
     print(result_img)
 
 
-heading_lab = Label(root, text="Internet Speed Test", font=(
-    'Times new roman', 30, 'bold'), bg='blue')
-heading_lab.place(x=60, y=40, height=50, width=380)
+heading_back = Label(root, bg='#252422', borderwidth=5, relief=SOLID)
+heading_back.place(x=0, y=0, height=100, width=600)
 
-up_speed_lab = Label(root, text="Download Speed",
-                     font=('Times new roman', 30, 'bold', ))
-up_speed_lab.place(x=40, y=130, height=50, width=380)
+heading_lab = Label(root, text="Internet Speed Test", font=(
+    'Time new roman', 30, 'bold'), bg='#252422', fg='#ffffff')
+heading_lab.place(x=60, y=30, height=50, width=480)
+
+down_speed_lab = Label(root, text="Download\nSpeed",
+                       font=('Times new roman', 23, 'bold'), bg='#4cc9f0')
+down_speed_lab.place(x=50, y=180, height=90, width=180)
 
 down_speed_result = Label(root, text="00", font=(
-    'Times new roman', 30, 'bold', ))
-down_speed_result.place(x=40, y=200, height=50, width=380)
+    'Times new roman', 25, 'bold', ), bg='#f2e9e4')
+down_speed_result.place(x=50, y=300, height=50, width=180)
 
-up_speed_lab = Label(root, text="Upload Speed",
-                     font=('Times new roman', 30, 'bold', ))
-up_speed_lab.place(x=40, y=290, height=50, width=380)
+up_speed_lab = Label(root, text="Upload\nSpeed",
+                     font=('Times new roman', 23, 'bold', ), bg='#4cc9f0')
+up_speed_lab.place(x=370, y=180, height=90, width=180)
 
 up_speed_result = Label(root, text="00", font=(
-    'Times new roman', 30, 'bold', ))
-up_speed_result.place(x=40, y=360, height=50, width=380)
+    'Times new roman', 25, 'bold', ), bg='#f2e9e4')
+up_speed_result.place(x=370, y=290, height=50, width=180)
 
-test_button = Button(root, text="Start Test", font=('Arial', 30, 'bold'),
-                     relief='ridge', bg='red', fg='white', command=speedcheck)
-test_button.place(x=40, y=460, height=50, width=380)
+test_button = Button(root, text="Start Test", font=('Arial', 30, 'bold'), bd=3,
+                     relief='ridge', bg='#22223b', fg='white', command=speedcheck)
+test_button.place(x=40, y=480, height=70, width=520)
 
 # mainloop at the end of the line which creates the tkinter window
 root.mainloop()
